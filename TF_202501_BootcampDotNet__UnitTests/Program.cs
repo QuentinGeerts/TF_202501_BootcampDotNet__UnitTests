1b3b347c837689b4bb730ffc50c6d06052d39bfe
QuentinGeerts <quentin.geerts@bstorm.be>
2025-01-21 11:16:43 +0100

    Ajoutez des fichiers projet.

---
+++ TF_202501_BootcampDotNet__UnitTests/Program.cs
@@ -0,0 +1,19 @@
+﻿//using TF_202501_BootcampDotNet__UnitTests.Models;
+
+
+//Console.WriteLine(Int32Extensions.IsPrime(3)); // true
+//Console.WriteLine(Int32Extensions.IsPrime(4)); // false
+//Console.WriteLine(Int32Extensions.IsPrime(5)); // true
+//Console.WriteLine(Int32Extensions.IsPrime(0)); // ArgumentException()...
+
+using Tools;
+
+int compteur = 0;
+
+while (compteur < 100)
+{
+    if (compteur.IsPrime())
+    {
+        Console.WriteLine();
+    }
+}
---
+++ Tools/Int32Extensions.cs
@@ -0,0 +1,20 @@
+﻿namespace Tools;
+
+public static class Int32Extensions
+{
+    public static bool IsPrime(this int number)
+    {
+        if (number < 1) throw new ArgumentException($"La valeur {number} ne peut pas être inférieure à 2.");
+        if (number == 2) return true;
+        if (number % 2 == 0) return false;
+
+        int boundary = (int)Math.Floor(Math.Sqrt(number));
+
+        for (int i = 3; i < boundary; i++)
+        {
+            if (number % i == 0) return false;
+        }
+
+        return true;
+    }
+}
