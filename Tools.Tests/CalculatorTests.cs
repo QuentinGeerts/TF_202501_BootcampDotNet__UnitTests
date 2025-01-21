b31071b93e834af18209a7c7a819de258b189285
QuentinGeerts <quentin.geerts@bstorm.be>
2025-01-21 14:22:08 +0100

    Démonstration unit tests avec instances

---
+++ Tools.Tests/CalculatorTests.cs
@@ -0,0 +1,34 @@
+﻿namespace Tools.Tests;
+
+public class CalculatorTests //: IDisposable
+{
+
+    private Calculator _calculator;
+
+    // Setup
+    public CalculatorTests()
+    {
+        _calculator = new Calculator();
+    }
+
+    //public void Dispose()
+    //{
+    //    _calculator = null;
+    //}
+
+    [Fact]
+    public void Addition_ShouldReturnAGoodValue ()
+    {
+        double result = _calculator.Addition(5, 5);
+
+        Assert.Equal(5, result);
+    }
+
+    [Fact]
+    public void Addition_ShouldReturnAWrongValue()
+    {
+        double result = _calculator.Addition(5, 4);
+
+        Assert.NotEqual(5, result);
+    }
+}
---
+++ Tools/Calculator.cs
@@ -0,0 +1,11 @@
+﻿namespace Tools;
+
+public class Calculator
+{
+    public double Addition(double x, double y) => x + y;
+    public double Division(double x, double y)
+    {
+        if (y == 0) throw new DivideByZeroException("Division par 0 impossible");
+        return x / y;
+    }
+}
